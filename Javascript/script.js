3250c1f9428c8990e4a962a4525b8dc632257bd5
Maruf Khan <marufatrai12@gmail.com>
2023-11-13 10:52:03 +0600

    JS-Class-5

---
+++ Javascript/script.js
@@ -0,0 +1,63 @@
+/*
+    var num = 50;
+
+    var num2 = toString(num);
+
+    document.write(typeof(num2));
+*/
+
+/*
+    var name = "maruf";
+
+    var name2 = parseInt(name);
+
+    document.write(typeof(name2));
+*/
+
+/*
+    var number = "5.70";
+
+    var number2 = parseFloat(number);
+
+    document.write(typeof(number2));
+
+*/
+
+/*
+    var numbers = 2.5678;
+
+    document.write(numbers.toFixed(1));
+
+*/
+
+
+
+/* 
+
+    var numbers = 2.5678;
+    document.write(numbers.toPrecision(1));
+
+*/
+
+/*
+
+    console.log(typeof(Number("15")));
+
+    console.log(typeof(Number("15.5")));
+*/
+
+/*
+
+    console.log( Number(true));
+
+    document.write(Number(false));
+
+*/
+
+
+
+
+
+
+
+
